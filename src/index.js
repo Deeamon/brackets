7d458812a71e88552ef3e77c82a9b69ff445b831
Marina Sugutova <marinasugutova@gmail.com>
2019-09-29 18:16:15 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let config = bracketsConfig;
+    let find  = config.toString().replace( /\,/g ,'');
+    let nstr = str;
+    let reg;
+    
+    for(let z = 0; z < find.length; z ++){
+        for (let j = 0; j < find.length; j+=2){
+            if(!isNaN (find)){
+                reg = new RegExp(find[j] + + find[j+1],'g');
+            }else{
+                reg = new RegExp('\\' +  find[j] +  '\\'  + find[j+1], 'g');
+            }
+            for(let i = 0; i < nstr.length; i++){
+                nstr = nstr.replace( reg, '');
+            }
+        }   
+    }
+              
+    return nstr ? false:true;
 }
